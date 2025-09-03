2e8b9d59b00ba4d49962cf78fb8f2d82a7b57108
bravend049-sudo <braven.d049@stu.nebo.edu>
2025-09-03 14:21:56 -0600

    Added marbles, trees immovable crate, and resized crates

---
+++ Tank Game Prot 1 (Old)/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public float Speed;
+    public float RotSpeed;
+
+    public float HInput;
+    public float VInput;
+
+    public float JumpVelocity;
+
+    public Rigidbody PlayerRB;
+
+    void Update()
+    {
+        HInput = Input.GetAxis("Horizontal");
+        VInput = Input.GetAxis("Vertical");
+
+        transform.Rotate(Vector3.up, HInput * RotSpeed * Time.deltaTime); //Horizontnal Rotation
+        transform.Translate(Vector3.forward * VInput * Speed * Time.deltaTime); //Forward and Backward Movement
+    }
+}
