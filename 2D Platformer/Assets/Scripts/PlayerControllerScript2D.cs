5f3b2e633652111c83911830162d3cfea25f5795
bravend049-sudo <braven.d049@stu.nebo.edu>
2025-10-10 14:18:18 -0600

    Added functionality
    Changelog:
    - Added player
    - Added platform
    - Added tileset

---
+++ 2D Platformer/Assets/Scripts/PlayerControllerScript2D.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class PlayerControllerScript2D : MonoBehaviour
+{
+
+    [Header("Player Settings")]
+    public float moveSpeed;
+    public float jumpForce;
+    public bool isGrounded;
+    public int bottomBound = -4;
+    [Header("Score")]
+    public int score;
+
+    public Rigidbody2D rig;
+    public TextMeshProUGUI scoreText;
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+        scoreText.text = "Score: " + score;
+    }
+
+    void FixedUpdate()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rig.linearVelocity = new Vector2(moveInput * moveSpeed, rig.linearVelocity.y);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            isGrounded = false;
+            rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        if (transform.position.y < bottomBound)
+        {
+            GameOver();
+        }
+    }
+
+    void OnCollision2D(Collision2D collision)
+    {
+        if (collision.GetContact(0).normal == Vector2.up)
+        {
+            isGrounded = true;
+        }
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
